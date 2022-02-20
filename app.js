d9680739bb983baec3ff76d35f552cd0723d8609
Alankrita08 <84913309+Alankrita08@users.noreply.github.com>
2022-02-20 16:25:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const API_URL = 'http://localhost:5000/api';
+$.get(`${API_URL}/devices`)
+  .then(response => {
+    response.forEach(device => {
+      $('#devices tbody').append(`
+      <tr>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
+
+      </tr>`
+      );
+    });
+  })
+  .catch(error => {
+    console.error(`Error: ${error}`);
+  });
+
+$('#add-device').on('click', () => {
+  const user = $('#user').val();
+  const name = $('#name').val();
+  const sensorData = [];
+
+  const body = {
+    user,
+    name,
+    sensorData
+  };
+
+  $.post(`${API_URL}/devices`, body)
+    .then(response => {
+      location.href = '/';
+    })
+    .catch(error => {
+      console.error(`Error: ${error}`);
+    });
+});
+
+$('#navbar').load('navbar.html');
